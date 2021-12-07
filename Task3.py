b0f1b41cb92e5f434b57e52aba90779f5667c7e7
Ethan Harris <576552241qwj@gmail.com>
2021-12-07 12:19:17 +0800

    main

--- Task3.py
+++ Task3.py
@@ -2,7 +2,7 @@
 from scapy.all import *
 eth = Ether(src="02:42:0a:09:00:06")
 ip  = IP(src="10.9.0.6", dst="10.9.0.5")
-tcp = TCP(sport=40262, dport=23, flags="A", seq=4249710848, ack=2605749234)
+tcp = TCP(sport=40276, dport=23, flags="A", seq=196255085, ack=137309990)
 data = "\r"
 pkt = ip/tcp/data
 ls(pkt)
